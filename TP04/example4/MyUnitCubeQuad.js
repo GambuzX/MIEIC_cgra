4f92f0a1aa72149dcd820b077fa0c00fdaa3e7c8
Mário Gil <mg_mesquita@hotmail.com>
2019-03-14 23:24:21 +0000

    MINECRAFT CUBE!!!!

--- TP04/example4/MyUnitCubeQuad.js
+++ TP04/example4/MyUnitCubeQuad.js
@@ -3,12 +3,12 @@ class MyUnitCubeQuad extends CGFobject {
 	constructor(scene) {
 		super(scene);
 		this.initBuffers();
-		this.initTexture();
+		this.initTextures();
 
 		this.myQuad = new MyQuad(scene);
 	}
 
-	initTexture() {
+	initTextures() {
 		this.mine = new CGFappearance(this.scene);
         this.mine.setAmbient(0.1, 0.1, 0.1, 1);
         this.mine.setDiffuse(0.9, 0.9, 0.9, 1);
@@ -16,18 +16,26 @@ class MyUnitCubeQuad extends CGFobject {
         this.mine.setShininess(10.0);
         this.mine.loadTexture('images/mineSide.png');
         this.mine.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.textureTop = new CGFtexture(this.scene,'images/mineTop.png');
+        this.textureBottom = new CGFtexture(this.scene,'images/mineBottom.png');
+        this.textureSide = new CGFtexture(this.scene,'images/mineSide.png');
 	}
 	setMineSide() {
-		this.mine.setTexture('images/mineSide.png');
+		this.mine.setTexture(this.textureSide);
 	}
 	setMineTop() {
-		this.mine.setTexture('images/mineTop.png');
+		this.mine.setTexture(this.textureTop);
 	}
 	setMineBottom() {
-		this.mine.setTexture('images/mineBottom.png');
+		this.mine.setTexture(this.textureBottom);
 	}
 
-	display() {	
+	display() {
+		this.setMineSide();
+		this.mine.apply();
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+
 		// Front
 		this.scene.pushMatrix();
 		this.scene.translate(0,0,0.5);
@@ -63,6 +71,9 @@ class MyUnitCubeQuad extends CGFobject {
 		angle = -Math.PI / 2;
 		this.scene.translate(0, 0.5, 0);
 		this.scene.rotate(angle, 1, 0, 0);
+		this.setMineTop();
+		this.mine.apply();
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.myQuad.display();
 		this.scene.popMatrix();
 
@@ -71,6 +82,9 @@ class MyUnitCubeQuad extends CGFobject {
 		angle = Math.PI / 2;
 		this.scene.translate(0, -0.5, 0);
 		this.scene.rotate(angle, 1, 0, 0);
+		this.setMineBottom();
+		this.mine.apply();
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.myQuad.display();
 		this.scene.popMatrix();
 	}
